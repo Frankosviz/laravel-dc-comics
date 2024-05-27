bef280fefc2d9f89388dac49b84630d05ec9fc92
Frankosviz <geom.francescopaolo@gmail.com>
2024-05-27 15:33:06 +0200

    Start commit

---
+++ database/migrations/2024_05_27_131515_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('author',100);
+            $table->string('description',250);
+            $table->string('thumb', 250);
+            $table->date('release_date');
+            $table->boolean('rare');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
